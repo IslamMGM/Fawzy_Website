62be7e67a5b631cbc4a9e55cb9c653dd5fbfa3ac
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2025-05-27 00:30:59 +0300

    Sixth response, trying to make menu pushes the page but it doesn't

--- js/header.js
+++ js/header.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
   const navToggle = document.querySelector('.nav-toggle');
   const mNav = document.querySelector('.m_nav');
+  const mainHeader = document.querySelector('.main-header');
   navToggle.addEventListener('click', function () {
     mNav.classList.toggle('active');
     navToggle.classList.toggle('open');
     document.body.classList.toggle('menu-open', mNav.classList.contains('active'));
+    mainHeader.classList.toggle('menu-open', mNav.classList.contains('active'));
   });
 });
